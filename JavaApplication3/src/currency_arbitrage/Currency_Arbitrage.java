9fd18973c6d71f26e66320eb3d122eb4aa620c2a
scoobaroo <scoobaroo415@gmail.com>
2017-03-23 20:40:25 -0700

    added bellman-ford algorithm and graph class, started making structure for Currencies as Nodes and exchange rates as edges

--- JavaApplication3/src/currency_arbitrage/Currency_Arbitrage.java
+++ JavaApplication3/src/currency_arbitrage/Currency_Arbitrage.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package currency_arbitrage;
-
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 import java.net.URL;
 import java.io.IOException;
 import java.net.URLClassLoader;
@@ -18,9 +20,10 @@ import java.util.Iterator;
 import java.util.Map;
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
-import org.json.JSONObject;
-import org.json.JSONArray;
-import com.sun.grizzly.utils; 
+import org.json.*;
+import com.sun.grizzly.utils.*; 
+import static java.lang.Math.log;
+
 /**
  *
  * @author suejanehan
@@ -126,7 +129,164 @@ public class Currency_Arbitrage {
 	return null;  // The request failed.
     } 
     public static void main(String[] args) {
+        Double BTCUSD = -Math.log(BTCUSD);
+        Double LTCBTC = -Math.log(LTCBTC);
+        Double BTCRUR = -Math.log(BTCRUR);
+        Double BTCEUR = -Math.log(BTCEUR);
         
+        Double LTCUSD = -Math.log(LTCUSD);
+        Double LTCRUR = -Math.log(LTCRUR);
+        Double LTCEUR = -Math.log(LTCEUR);
+        
+        Double NMCBTC = -Math.log(NMCBTC);
+        Double NMCUSD = -Math.log(NMCUSD);
+        
+        Double NVCBTC = -Math.log(NVCBTC);
+        Double NVCUSD = -Math.log(NVCUSD);
+        
+        Double USDRUR = -Math.log(USDRUR);
+        Double EURUSD = -Math.log(EURUSD);
+        Double EURRUR = -Math.log(EURRUR);
+        
+        Double PPCBTC = -Math.log(PPCBTC);
+        
+        Double DSHBTC = -Math.log(DSHBTC);
+        Double DSHUSD = -Math.log(DSHUSD);
+        Double DSHRUR = -Math.log(DSHRUR);
+        Double DSHEUR = -Math.log(DSHEUR);
+        Double DSHLTC = -Math.log(DSHLTC);
+        Double DSHETH = -Math.log(DSHETH);
+        
+        Double ETHBTC = -Math.log(ETHBTC);
+        Double ETHUSD = -Math.log(ETHUSD);
+        Double ETHRUR = -Math.log(ETHRUR);
+        Double ETHEUR = -Math.log(ETHEUR);
+        Double ETHLTC = -Math.log(ETHLTC);
+        
+        ArrayList currencies;
+        currencies = new ArrayList<Object>();
+        currencies.add("BTC");
+        currencies.add("USD");
+        currencies.add("LTC");
+        currencies.add("RUR");
+        currencies.add("EUR");
+        currencies.add("NMC");
+        currencies.add("NVC");
+        currencies.add("PPC");
+        currencies.add("DSH");
+        currencies.add("ETH");
+                
+        Graph graph;
+        graph = new Graph(currencies,25);
+        
+        // add edge BTCUSD
+        graph.edge[0].src = (String) currencies.get(0);
+        graph.edge[0].dest = (String) currencies.get(1);
+        graph.edge[0].weight = BTCUSD;
+        // add edge LTCBTC
+        graph.edge[1].src = (String) currencies.get(2);
+        graph.edge[1].dest = (String) currencies.get(0);
+        graph.edge[1].weight = LTCBTC;
+        // add edge BTCRUR
+        graph.edge[2].src = (String) currencies.get(0);
+        graph.edge[2].dest = (String) currencies.get(3);
+        graph.edge[2].weight = BTCRUR;
+        // add edge BTCEUR
+        graph.edge[3].src = (String) currencies.get(0);
+        graph.edge[3].dest = (String) currencies.get(4);
+        graph.edge[3].weight = BTCEUR;
+        // add edge LTCUSD
+        graph.edge[4].src = (String) currencies.get(2);
+        graph.edge[4].dest = (String) currencies.get(1);
+        graph.edge[4].weight = LTCUSD;
+        // add edge LTCRUR
+        graph.edge[5].src = (String) currencies.get(2);
+        graph.edge[5].dest = (String) currencies.get(3);
+        graph.edge[5].weight = LTCRUR;
+        // add edge LTCEUR
+        graph.edge[6].src = (String) currencies.get(2);
+        graph.edge[6].dest = (String) currencies.get(4);
+        graph.edge[6].weight = LTCEUR;
+        // add edge NMCBTC
+        graph.edge[7].src = (String) currencies.get(5);
+        graph.edge[7].dest = (String) currencies.get(0);
+        graph.edge[7].weight = NMCBTC;
+        // add edge NMCUSD
+        graph.edge[8].src = (String) currencies.get(5);
+        graph.edge[8].dest = (String) currencies.get(1);
+        graph.edge[8].weight = NMCUSD;
+        // add edge NVCBTC
+        graph.edge[9].src = (String) currencies.get(6);
+        graph.edge[9].dest = (String) currencies.get(0);
+        graph.edge[9].weight = NVCBTC;
+        // add edge NVCUSD
+        graph.edge[10].src = (String) currencies.get(6);
+        graph.edge[10].dest = (String) currencies.get(1);
+        graph.edge[10].weight = NVCUSD;
+        
+        ///NOT DONE YET///
+        // add edge xxxxxx
+        graph.edge[11].src = (String) currencies.get(0);
+        graph.edge[11].dest = (String) currencies.get(0);
+        graph.edge[11].weight = NMCBTC;
+        // add edge xxxxxx
+        graph.edge[12].src = (String) currencies.get(0);
+        graph.edge[12].dest = (String) currencies.get(0);
+        graph.edge[12].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[13].src = (String) currencies.get(0);
+        graph.edge[13].dest = (String) currencies.get(0);
+        graph.edge[13].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[14].src = (String) currencies.get(0);
+        graph.edge[14].dest = (String) currencies.get(0);
+        graph.edge[14].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[15].src = (String) currencies.get(0);
+        graph.edge[15].dest = (String) currencies.get(0);
+        graph.edge[15].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[16].src = (String) currencies.get(0);
+        graph.edge[16].dest = (String) currencies.get(0);
+        graph.edge[16].weight = NMCBTC;
+                // add edge NMCBTC
+        graph.edge[17].src = (String) currencies.get(0);
+        graph.edge[17].dest = (String) currencies.get(0);
+        graph.edge[17].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[18].src = (String) currencies.get(0);
+        graph.edge[18].dest = (String) currencies.get(0);
+        graph.edge[18].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[19].src = (String) currencies.get(0);
+        graph.edge[19].dest = (String) currencies.get(0);
+        graph.edge[19].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[20].src = (String) currencies.get(0);
+        graph.edge[20].dest = (String) currencies.get(0);
+        graph.edge[20].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[21].src = (String) currencies.get(0);
+        graph.edge[21].dest = (String) currencies.get(0);
+        graph.edge[21].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[22].src = (String) currencies.get(0);
+        graph.edge[22].dest = (String) currencies.get(0);
+        graph.edge[22].weight = NMCBTC;
+        // add edge xxx
+        graph.edge[23].src = (String) currencies.get(0);
+        graph.edge[23].dest = (String) currencies.get(0);
+        graph.edge[23].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[24].src = (String) currencies.get(0);
+        graph.edge[24].dest = (String) currencies.get(0);
+        graph.edge[24].weight = NMCBTC;
+        // add edge NMCBTC
+        graph.edge[25].src = (String) currencies.get(0);
+        graph.edge[25].dest = (String) currencies.get(0);
+        graph.edge[25].weight = NMCBTC;
+ 
+ 
     }
     
 }
---
+++ JavaApplication3/src/currency_arbitrage/Graph.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package currency_arbitrage;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author suejanehan
+ */
+public class Graph
+{
+    // A class to represent a weighted edge in graph
+    class Edge {
+        String src, dest;
+        Double weight;
+        Edge() {
+            src = dest = "";
+            weight = 0.0;
+        }
+    };
+ 
+    int E;
+    ArrayList<String> V;
+    Edge edge[];
+ 
+    // Creates a graph with V vertices and E edges
+    Graph(ArrayList<String> v, int e)
+    {
+        V = v;
+        E = e;
+        edge = new Edge[e];
+        for (int i=0; i<e; ++i)
+            edge[i] = new Edge();
+    }
+ 
+    // The main function that finds shortest distances from src
+    // to all other vertices using Bellman-Ford algorithm.  The
+    // function also detects negative weight cycle
+    void BellmanFord(Graph graph,int src)
+    {
+        ArrayList<String> V = graph.V;
+        E = graph.E;
+        double dist[] = new double[V];
+ 
+        // Step 1: Initialize distances from src to all other
+        // vertices as INFINITE
+        for (int i=0; i<V.size(); ++i)
+            dist[i] = Integer.MAX_VALUE;
+        dist[src] = 0;
+ 
+        // Step 2: Relax all edges |V| - 1 times. A simple
+        // shortest path from src to any other vertex can
+        // have at-most |V| - 1 edges
+        for (int i=1; i<V.size(); ++i)
+        {
+            for (int j=0; j<E; ++j)
+            {
+                String u = graph.edge[j].src;
+                String v = graph.edge[j].dest;
+                double weight = graph.edge[j].weight;
+                if (dist[u]!=Integer.MAX_VALUE &&
+                    dist[u]+weight<dist[v])
+                    dist[v]=dist[u]+weight;
+            }
+        }
+ 
+        // Step 3: check for negative-weight cycles.  The above
+        // step guarantees shortest distances if graph doesn't
+        // contain negative weight cycle. If we get a shorter
+        //  path, then there is a cycle.
+        for (int j=0; j<E; ++j)
+        {
+            String u = graph.edge[j].src;
+            String v = graph.edge[j].dest;
+            double weight = graph.edge[j].weight;
+            if (dist[u]!=Integer.MAX_VALUE &&
+                dist[u]+weight<dist[v])
+              System.out.println("Graph contains negative weight cycle");
+        }
+        printArr(dist, V);
+    }
+ 
+    // A utility function used to print the solution
+    void printArr(int dist[], int V)
+    {
+        System.out.println("Vertex   Distance from Source");
+        for (int i=0; i<V; ++i)
+            System.out.println(i+"\t\t"+dist[i]);
+    }
+ 
+    // Driver method to test above function
+    public static void main(String[] args)
+    {
+        int V = 5;  // Number of vertices in graph
+        int E = 8;  // Number of edges in graph
+ 
+        Graph graph = new Graph(V, E);
+        
+        // add edge 0-1 (or A-B in above figure)
+        graph.BellmanFord(graph, 0);
+    }
+}
